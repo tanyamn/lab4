c0585ec551e4a0a0e21a6d431b946b36f00f96db
Tanya Nguyen <tmn051@ucsd.edu>
2019-01-31 10:49:54 -0800

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,10 @@
+
+exports.viewProject = function(req, res) {
+
+  	var name = req.params.name; 
+  	console.log("The project name is: " + name);
+	res.render('project', {
+    	'projectName': name
+  });
+
+}
